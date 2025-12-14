62fd0d2cf143c404fd7a3143b19133df40958886
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-14 17:45:04 +0200

    Add project files.

---
+++ HomeWork1/HomeWork1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "*********************************\n";
+    std::cout << "Name: Mohammed Alhaj Mohammed,\n";
+    std::cout << "Age: 23 Years,\n";
+    std::cout << "City: Palastine,\n";
+    std::cout << "country: Gaza.\n";
+    std::cout << "*********************************\n";
+}
+
