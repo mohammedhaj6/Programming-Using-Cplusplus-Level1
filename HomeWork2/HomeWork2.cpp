14a13a7cb310605d0943a8a76718633fb7acd32a
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-15 11:47:41 +0200

    add homework2

---
+++ HomeWork2/HomeWork2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "*********************************\n";
+   cout << "Name: Mohammed Alhaj Mohammed,\n";
+   cout << "Age: 23 Years,\n";
+   cout << "City: Palastine,\n";
+   cout << "country: Gaza.\n";
+   cout << "*********************************\n";
+}
