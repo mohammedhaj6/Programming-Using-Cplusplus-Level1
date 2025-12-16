2b362e8bec1a58b4f0a801470fa436ecd81683bf
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-16 13:19:33 +0200

    add HomeWork3

---
+++ HomeWork3/HomeWork3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//HomeWork3
+	cout << "\a";
+	//-----------------------------
+
+	cout << "DearSir\\Madam,\n";
+	cout << "How are you?\n";
+	cout << "My name is \"Mohammed\",nice to meet you.\n";
+
+	//-----------------------------
+
+	cout << "Ali\tAhmed\tLina\n";
+	cout << "Fadi\tZain\tMona\n";
+
+
+}
+
