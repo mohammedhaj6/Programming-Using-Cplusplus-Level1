81649c06f417de3b3acc51a19b2f8acbba1bd4c5
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-17 16:36:09 +0200

    add HomeWork4

---
+++ HomeWork4/HomeWork4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+    string name = "mohammed alhaj mohammed";
+    int age = 23;
+    string city = "gaza";
+    string country = "palestine";
+    int monthSalary = 5000;
+    int yearSalary = monthSalary * 12;
+    char gender = 'M';
+    bool married = 1;
+
+    cout <<"*************************************" << "\n";
+    cout << "Name: " << name << "\n";
+    cout << "Age: " << age <<" Years" << "\n";
+    cout << "City: " << city << "\n";
+    cout << "Country: " << country << "\n";
+    cout << "Monthly Salary: " << monthSalary << "\n";
+    cout << "Yearly Salary: " << yearSalary << "\n";
+    cout << "Gender: " << gender << "\n";
+    cout << "Married: " << married<<"\n";
+    cout << "***********************************" << "\n" << "\n";
+
+
+    cout << "-----------------------------------------------------" << "\n" << "\n";
+
+    int x = 20, y = 30, z = 10;
+    int total = x + y + z;
+
+    cout << x << "+"<<"\n";
+    cout << y << "+" << "\n";
+    cout << z << "\n";
+    cout << "---------------------------------" << "\n";
+
+    cout<<"Total = " << total <<"\n";
+
+
+}
