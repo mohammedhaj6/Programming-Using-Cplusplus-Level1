ab0c354dfc0efd368d064cd33fe9e1797e84cddd
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-20 11:58:35 +0200

    add HomeWork 6

---
+++ HomeWork6/HomeWork6.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ // homework 1
+	//short A,B;
+
+	//    cout << "Please enter the first number A? \n";
+	//    cin >> A;
+	//	cout << "Please enter the first number B? \n";
+	//	cin >> B;
+	//	cout << "\n\n";
+	//	cout << A << " + " << B << " = " << A + B<<"\n";
+	//	cout << A << " - " << B << " = " << A - B << "\n";
+	//	cout << A << " * " << B << " = " << A * B << "\n";
+	//	cout << A << " / " << B << " = " << A / B << "\n";
+	//	cout << A << " % " << B << " = " << A % B << "\n";
+	//	cout << "-----------------------------------------------";
+
+		// problem 7
+		//short num;
+		//cout << "Enter your number ? \n";
+		//cin >> num;
+		//cout << "\n";
+		//cout << "Half of the " << num << " is " << num/2 <<"\n";
+		//cout << "-----------------------------------------------";
+		
+	 //problem 9
+
+	/*short num1, num2, num3;
+	cout << "Enter your number 1 \n";
+	cin >> num1;
+	cout << "Enter your number 2 \n";
+	cin >> num2;
+	cout << "Enter your number 3 \n";
+	cin >> num3;
+	cout << "\n";
+	cout << "The sum of enterd numbers >>> " << num1 + num2 + num3;*/
+	     
+	//propblem 10
+	/*short mark1, mark2, mark3;
+	cout << "Enter your mark 1 \n";
+	cin >> mark1;
+	cout << "Enter your mark 2 \n";
+	cin >> mark2;
+	cout << "Enter your mark 3 \n";
+	cin >> mark3;
+	cout << "\n";
+	cout << "The average of enterd numbers >>> " << (mark1 + mark2 + mark3)/3;*/
+    //problem 14 
+	/*short num1, num2 ,temp;
+	cout << "Enter your number 1 \n";
+	cin >> num1;
+	cout << "Enter your number 2 \n";
+	cin >> num2;
+	cout << "\n";
+	cout << num1<<"\n";
+	cout << num2 << "\n";
+	cout << "\n\n";
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+	cout << num1 << "\n";
+	cout << num2 << "\n";*/
+	//problem 15
+	/*short num1, num2;
+	cout << "Enter your number 1 \n";
+	cin >> num1;
+	cout << "Enter your number 2 \n";
+	cin >> num2;
+	cout << "\n";
+	cout << "The rectangle area >>> " << num1 * num2;*/
+	//problem 17
+	/*short a, h;
+	cout << "Enter your number 1 \n";
+	cin >> a;
+	cout << "Enter your number 2 \n";
+	cin >> h;
+	cout << "\n";
+	cout << "The triangle area >>> " << (0.5*a) * h;*/
+
+	//problem 19
+	/*short D;
+    const double PI = 3.14;
+
+	cout << "Enter your diameter \n";
+	cin >> D;
+	
+	cout << "\n";
+	cout << "The rectangle area >>> " << (PI * D * D)/4;*/
+
+	//problem 20
+	/*short A;
+	const double PI = 3.14;
+
+	cout << "Enter your diameter \n";
+	cin >> A;
+
+	cout << "\n";
+	cout << "The rectangle area >>> " << PI*(A*A)/4;*/
+
+	//problem 21
+
+
+
+
+
+}
+
