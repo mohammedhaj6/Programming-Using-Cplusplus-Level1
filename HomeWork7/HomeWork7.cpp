28b2615256b9a261856b0e27d4dcf6aad5bb5b33
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-22 16:54:31 +0200

    add HomeWork7

---
+++ HomeWork7/HomeWork7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Please enter the first number A?\n";
+	cin >> a;
+	cout << "Please enter the first number B?\n";
+	cin >> b;
+	cout << "\n\n";
+	cout << a << " = " << b << " is " << (a==b)<<"\n";
+	cout << a << " != " << b << " is " << (a != b) << "\n";
+	cout << a << " > " << b << " is " << (a > b) << "\n";
+	cout << a << " < " << b << " is " << (a < b) << "\n";
+	cout << a << " >= " << b << " is " << (a >= b) << "\n";
+	cout << a << " <= " << b << " is " << (a <= b) << "\n";
+
+
+
+
+}
+
