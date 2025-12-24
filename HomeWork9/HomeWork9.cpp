69eff489f017a5740524c71e52563d4315b21700
ETO <mohammedalhajmohammed6@gmail.com>
2025-12-24 18:25:04 +0200

    add HomeWork9

---
+++ HomeWork9/HomeWork9.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	//problem 16
+	//short a, d;
+	//cout << "Enter your number 1 \n";
+	//cin >> a;
+	//cout << "Enter your number 2 \n";
+	//cin >> d;
+	//cout << "\n";
+	//cout << "The rectangle area >>> " <<a*sqrt(pow(d,2)-pow(a,2)) ;
+
+	//problem 18
+	/*short r;
+	const double PI = 3.14;
+
+	cout << "Enter your radius \n";
+	cin >> r;
+
+	cout << "\n";
+	cout << "The circle area >>> " << ceil( PI*pow(r,2));*/
+
+	//problem 19
+
+	/*short D;
+	const double PI = 3.14;
+
+	cout << "Enter your diameter \n";
+	cin >> D;
+	cout << "\n";
+	cout << "The circle area >>> " << ceil((PI * pow(D,2)) / 4);*/
+
+	//problem 20
+
+	//short A;
+	//const double PI = 3.14;
+
+	//cout << "Enter  Side length of the square  \n";
+	//cin >> A;
+
+	//cout << "\n";
+	//cout << "The circle area >>> " << ceil(PI * (pow(A,2)) / 4);
+
+	//problem 21
+
+	//short L;
+	//const double PI = 3.14;
+
+	//cout << "Enter your circumference \n";
+	//cin >> L;
+
+	//cout << "\n";
+	//cout << "The circle area >>> " << floor((pow(L,2))/(4*PI));
+
+  //problem 22
+  //short a,b;
+  //  const double PI = 3.14;
+
+  //cout << "Enter a : \n";
+  //cin >> a;
+  //cout << "Enter b : \n";
+  //cin >> b;
+
+  //cout << "\n";
+  //cout << "The circle area >>> " << floor(PI *((pow(b,2) / 4) * (2 * a - b) / (2 * a + b)));
+
+	// problem 23
+
+
+
+
+
+
+}
+
