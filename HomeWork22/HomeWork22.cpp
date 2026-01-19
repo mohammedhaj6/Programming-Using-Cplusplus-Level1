2b4bad7ac4d2807c6146c144658937392e9f37ee
ETO <mohammedalhajmohammed6@gmail.com>
2026-01-19 16:45:04 +0200

    add HomeWork22 , if else

---
+++ HomeWork22/HomeWork22.cpp
@@ -0,0 +1,93 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*problem 4
+	int age;
+	bool driverLicense;
+
+	cout << "enter your age ?\n";
+	cin >> age;
+
+	cout << "do you have a driver's license ?,enter '1' if 'yes' , enter '0' if 'no' \n";
+	cin >> driverLicense;
+
+	if (age > 21 && driverLicense==1) {
+		cout << "Hired\n";
+	}
+	else {
+		cout << "Rejected\n";
+	}*/
+
+	/*problem 8
+
+	int mark;
+
+	cout << "enter your mark ?\n";
+	cin >> mark;
+
+	if(mark>=50) {
+		cout << "Pass\n";
+	}
+	else
+	{
+		cout << "Fail\n";
+	}*/
+
+	/*problem 11
+
+	int mark1,mark2,mark3;
+
+	cout << "enter your mark 1 ?\n";
+	cin >> mark1;
+	cout << "enter your mark 2 ?\n";
+	cin >> mark2;
+	cout << "enter your mark 3 ?\n";
+	cin >> mark3;
+	cout << "------------------------------------------\n";
+	int avg = (mark1 + mark2 + mark3) / 3;
+
+	if (avg >= 50) {
+		cout << avg << "\n";
+		cout << "Pass\n";
+	}
+	else
+	{
+		cout << "Fail\n";
+	}*/
+
+	/*problem 24
+
+	int age;
+
+	cout << "enter your mark ?\n";
+	cin >> age;
+
+	if (age>=18 && age<= 45) {
+		cout << "Valid Age\n";
+	}
+	else
+	{
+		cout << "Invalid Age\n";
+	}*/
+
+	/*problem 45
+
+	string pin;
+
+	cout << "enter the password ?\n";
+	cin >> pin;
+
+	if (pin == "1234") {
+		cout << "Your balanced is : 7500\n";
+	}
+	else
+	{
+		cout << "Wrong PIN\n";
+	}*/
+
+
+}
+
