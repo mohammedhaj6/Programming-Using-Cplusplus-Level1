e6513d65bcdbf259bd97ce907dc83fcbda7095a7
ETO <mohammedalhajmohammed6@gmail.com>
2026-01-21 15:56:19 +0200

    add HomeWork 23 , else if

---
+++ HomeWork23/HomeWork23.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*problem 33
+
+	int grade;
+
+	cout << "Enter Your Grade ?\n";
+	cin >> grade;
+	cout << "\n";
+
+	if (grade>=90&& grade<=100) {
+		cout << "A\n";
+	}
+	else if (grade >= 80 && grade <= 89) {
+		cout << "B\n";
+	}
+	else if (grade >= 70 && grade <= 79) {
+		cout << "C\n";
+	}
+	else if (grade >= 60 && grade <= 69) {
+		cout << "D\n";
+	}
+	else if (grade >= 50 && grade <= 59) {
+		cout << "E\n";
+	}
+	else
+	{
+		cout << "F\n";
+	}*/
+
+	/*problem 34
+
+	int totalSales;
+
+	cout << "Enter Your Total Sales ?\n";
+	cin >> totalSales;
+	cout << "\n";
+	if (totalSales == 1000000) {
+		cout << totalSales * 0.01;
+		cout << "\n";
+	}
+	else if (totalSales > 500000 && totalSales < 1000000) {
+		cout << totalSales * 0.02;
+		cout << "\n";
+	}
+	else if (totalSales > 100000 && totalSales < 500000) {
+		cout << totalSales * 0.03;
+			cout << "\n";
+	}
+	else if (totalSales > 50000 && totalSales < 100000) {
+		cout << totalSales * 0.05;
+		cout << "\n";
+	}
+	else
+	{
+		cout << "0 \n";
+	}*/
+
+	/*problem 36
+
+	int num1,num2;
+	string operationType;
+
+	cout << "Enter Number 1 ?\n";
+	cin >> num1;
+	cout << "\n";
+	cout << "Enter Number 2 ?\n";
+	cin >> num2;
+	cout << "\n";
+	cout << "Enter Operation Type ?\n";
+	cin >> operationType;
+	cout << "\n";
+
+	if (operationType == "+") {
+		cout << num1 + num2<<"\n";
+	}
+	else if (operationType == "*") {
+		cout << num1 * num2 << "\n";
+	}
+	else if (operationType == "-") {
+		cout << num1 - num2 << "\n";
+	}
+	else if (operationType == "/") {
+		cout << num1 / num2 << "\n";
+	}
+	else {
+		cout << "Error !";
+	}*/
+
+	/*problem 44
+
+	int day;
+	
+
+	cout << "Enter Your Day ?\n";
+	cin >> day;
+	cout << "\n";
+	
+
+	if (day == 1) {
+		cout << "Sunday\n";
+	}
+	else if (day == 2) {
+		cout << "Monday\n";
+	}
+	else if (day == 3) {
+		cout << "Tuesday\n";
+	}
+	else if (day == 4) {
+		cout << "Wednesday\n";
+	}
+	else if (day == 5) {
+		cout << "Thursday\n";
+	}
+	else if (day == 6) {
+		cout << "Friday\n";
+	}
+	else if (day == 7) {
+		cout << "Saturday\n";
+	}
+	else {
+		cout << "Wrong Day !";
+	}*/
+
+//problem 45
+//
+//int month;
+//
+//
+//cout << "Enter Your Month ?\n";
+//cin >> month;
+//cout << "\n";
+//
+//
+//if (month == 1) {
+//	cout << "January\n";
+//}
+//else if (month == 2) {
+//	cout << "Febuary\n";
+//}
+//else if (month == 3) {
+//	cout << "March\n";
+//}
+//else if (month == 4) {
+//	cout << "April\n";
+//}
+//else if (month == 5) {
+//	cout << "May\n";
+//}
+//else if (month == 6) {
+//	cout << "June\n";
+//}
+//else if (month == 7) {
+//	cout << "July\n";
+//}
+//else if (month == 8) {
+//	cout << "August\n";
+//}
+//else if (month == 9) {
+//	cout << "September\n";
+//}
+//else if (month == 10) {
+//	cout << "October\n";
+//}
+//else if (month == 11) {
+//	cout << "November\n";
+//}
+//else if (month == 12) {
+//	cout << "December\n";
+//}
+//else {
+//	cout << "Wrong month !";
+//}
+
+}
+
