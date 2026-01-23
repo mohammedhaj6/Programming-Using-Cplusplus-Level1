3c2507ea949a8579c8cb724576dede4359f15243
ETO <mohammedalhajmohammed6@gmail.com>
2026-01-23 18:00:28 +0200

    add HomeWork24,Switch .. Case Statement

---
+++ HomeWork24/HomeWork24.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	//	problem 36
+
+	/*int num1, num2;
+	char operationType;
+
+	cout << "Enter Number 1 ?\n";
+	cin >> num1;
+	cout << "\n";
+	cout << "Enter Number 2 ?\n";
+	cin >> num2;
+	cout << "\n";
+	cout << "Enter Operation Type ?\n";
+	cin >> operationType;
+	cout << "\n";
+
+	switch (operationType) {
+	case '+':
+		cout << num1 + num2 << "\n";
+		break;
+	case '*':
+		cout << num1 * num2 << "\n";
+		break;
+	case '-':
+		cout << num1 - num2 << "\n";
+		break;
+	case '/':
+		cout << num1 / num2 << "\n";
+		break;
+	default:
+		cout << "Error Inputs !\n";
+
+	}*/
+
+
+	//problem 44
+
+	/*int day;
+
+
+	cout << "Enter Your Day ?\n";
+	cin >> day;
+	cout << "\n";
+
+
+	switch (day) {
+	case 1 :
+		cout << "Sunday\n";
+		break;
+	case 2:
+		cout << "Monday\n";
+		break;
+	case 3:
+		cout << "Tuesday\n";
+		break;
+	case 4:
+		cout << "Wednesday\n";
+		break;
+	case 5:
+		cout << "Thursday\n";
+		break;
+	case 6:
+		cout << "Friday\n";
+		break;
+	case 7:
+		cout << "Saturday\n";
+		break;
+	 default:
+		 cout << "Wrong Day !\n";
+
+	}*/
+
+
+	//problem 45
+
+	/*int month;
+
+
+	cout << "Enter Your Month ?\n";
+	cin >> month;
+	cout << "\n";
+
+
+	switch (month) {
+	case 1:
+		cout << "January\n";
+		break;
+	case 2:
+		cout << "Febuary\n";
+		break;
+	case 3:
+		cout << "March\n";
+		break;
+	case 4:
+		cout << "April\n";
+		break;
+	case 5:
+		cout << "May\n";
+		break;
+	case 6:
+		cout << "June\n";
+		break;
+	case 7:
+		cout << "July\n";
+		break;
+	case 8:
+		cout << "August\n";
+		break;
+	case 9:
+		cout << "September\n";
+		break;
+	case 10:
+		cout << "October\n";
+		break;
+	case 11:
+		cout << "November\n";
+		break;
+	case 12:
+		cout << "December\n";
+		break;
+	default:
+		cout << "Wrong month!\n";
+
+
+	}*/
+}
