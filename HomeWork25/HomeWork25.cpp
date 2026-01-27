b476d7dc19244625a55ec05f272c4298d7966473
ETO <mohammedalhajmohammed6@gmail.com>
2026-01-27 13:20:51 +0200

    add HomeWork 25 , For Loops

---
+++ HomeWork25/HomeWork25.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // problem 26
+	//int N;
+	//cout << "Enter The Number ?\n";
+	//cin >> N;
+	//cout << "\n";
+
+	//for (int i = 1; i <= N; i++) 
+	//{
+	//	cout << i <<"\n";
+	//}
+
+	// problem 27
+
+	/*int N;
+	cout << "Enter The Number ?\n";
+	cin >> N;
+	cout << "\n";
+
+	for (int i = N; i >= 1; i--) 
+	{
+		cout << i <<"\n";
+	}*/
+
+
+	// problem 28
+
+	//int N,sum=0;
+	//cout << "Enter The Number ?\n";
+	//cin >> N;
+	//cout << "\n";
+	//for (int  i = 1;i <= N; i++)
+	//{
+	//	if (i % 2 != 0) {
+	//		
+	//		sum += i;
+	//		
+	//	}
+	//	
+	//}
+	//cout << sum << "\n";
+
+	// problem 29
+	
+	/*int N,sum=0;
+	cout << "Enter The Number ?\n";
+	cin >> N;
+	cout << "\n";
+	for (int  i = 1;i <= N; i++)
+	{
+		if (i % 2 == 0) {
+			
+			sum += i;
+			
+		}
+		
+	}
+	cout << sum << "\n";*/
+
+	// problem 30
+
+	/*int N,F=1;
+	cout << "Enter The Number ?\n";
+	cin >> N;
+	cout << "\n";
+	for (int i = 1;i <= N; ++i)
+	{
+		F *= i;
+		
+	}
+	cout << F << "\n";*/
+	
+
+	// problem 32
+
+	/*int N,M,result=1;
+	cout << "Enter The Number ?\n";
+	cin >> N;
+	cout << "Enter The Power ?\n";
+	cin >> M;
+	cout << "\n";
+	for (int i = 1;i <= M; ++i)
+	{
+		 
+		result *= N;
+	}
+	
+	cout << result <<"\n";*/
+
+	// problem 46
+
+	
+	/*for (int i = 65; i <=90; i++)
+	{
+
+		cout << char(i) <<"\n";
+	}*/
+
+}
+
