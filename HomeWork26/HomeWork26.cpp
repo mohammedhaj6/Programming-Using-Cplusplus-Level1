74f9eb61bd2f5b376faa2cb1e703e3e00ab1d4c9
ETO <mohammedalhajmohammed6@gmail.com>
2026-01-28 14:47:30 +0200

    add HomeWork 26 , Nasted Loop

--- HomeWork26/HomeWork26.cpp
+++ HomeWork26/HomeWork26.cpp
@@ -1,20 +1,84 @@
-// HomeWork26.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	/*for (int i = 0;  i <= 10;  i++)
+	{
+		cout << "i = " << i <<"\n \n";
+
+		for (int j = 0; j <= 10; j++)
+		{
+
+			cout << i << " * " << j << " = " << i * j <<"\n";
+
+
+		}
+		cout << "----------------------------------------- \n";
+	}*/
+
+
+	//HomeWork 26
+
+	/*for (int i = 65; i <=90; i++)
+	{
+
+
+
+		for (int j = 65; j <= 90;j++) {
+			cout << char(i) << char(j)<<"\n";
+		}
+		cout << "-------------------------------------\n";
+	}*/
+
+	/////////////////////////////////////////////////////////////
+
+
+
+	/*for (int i = 10; i >= 1 ; i--)
+	{
+		for (int j = 1; j <= i;j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}*/
+
+	/////////////////////////////////////////////////////////////
+
+
+
+	/*for (int i = 10; i >= 1 ; i--)
+	{
+		for (int j = 1; j <= i;j++) {
+			cout << j;
+		}
+		cout << "\n";
+	}*/
+
+	/////////////////////////////////////////////////////////////
+
+	/*for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= i; j++) {
+			cout << j;
+		}
+		cout << "\n";
+
+	}*/
+
+	/////////////////////////////////////////////////////////////
+     
+
+
+	/*for (int i = 65; i <=70; i++)
+	{
+		for (int j = 65; j <= i;j++) {
+			cout <<char(j);
+		}
+		cout << "\n";
+	}*/
+
+	
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
