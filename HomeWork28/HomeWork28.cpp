1e2ad67b564b4c594ebc38a8e422817f92a9dce9
ETO <mohammedalhajmohammed6@gmail.com>
2026-01-31 17:40:58 +0200

    add HomeWork 28 , break and continue statements

---
+++ HomeWork28/HomeWork28.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // break HomeWork
+   /* int arr[10] = { 10,20,44,55,33,22,99,88,99,100 };
+    
+    for (int i = 0; i <= 10; i++) {
+        cout << arr[i]<<"\n";
+        if (arr[i] == 20) {
+            cout << "its position\n";
+
+            break;
+        }
+
+    }*/
+    //////////////////////////////////////////////////////
+
+    //continue HomeWork
+
+   /* int num,sum=0;
+  
+   
+    for (int i = 0; i < 5;i++) {
+        
+        cout << "Enter The Number ? \n";
+        cin >> num;
+
+        if (num>50) {
+            continue;
+        }
+        sum += num;
+    }
+
+    cout << "\n";
+    cout <<"The Output : " << sum << "\n";*/
+}
+
